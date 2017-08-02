24474b036273523ef63c45a2461615a5382cc3bc
Ricardo Breßler <r.bressler@makroground.de>
2017-08-02 16:29:51 +0200

    Abrufen der Daten und Splitten in: - Tag, 9 -9 Uhr, 9 - 22 Uhr

---
+++ ApoInfo/ApoInfo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ApoInfo
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.btn_get = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Location = new System.Drawing.Point(12, 38);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(35, 13);
+            this.lbl_result.TabIndex = 0;
+            this.lbl_result.Text = "label1";
+            // 
+            // btn_get
+            // 
+            this.btn_get.Location = new System.Drawing.Point(12, 12);
+            this.btn_get.Name = "btn_get";
+            this.btn_get.Size = new System.Drawing.Size(75, 23);
+            this.btn_get.TabIndex = 1;
+            this.btn_get.Text = "Abrufen";
+            this.btn_get.UseVisualStyleBackColor = true;
+            this.btn_get.Click += new System.EventHandler(this.btn_get_Click);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1385, 726);
+            this.Controls.Add(this.btn_get);
+            this.Controls.Add(this.lbl_result);
+            this.Name = "frm_main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Button btn_get;
+    }
+}
+
---
+++ ApoInfo/ApoInfo/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApoInfo
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_get_Click(object sender, EventArgs e)
+        {
+
+            lbl_result.Text = "";
+
+            string result = string.Empty;
+            string url = @"http://oldenburger-apotheken.de/notdienst";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+
+
+            // Datensatz fuer den heutigen Tag extrahieren
+
+
+
+            string[] delimiters1 = new string[] { "<TR>", "</TR>" };
+            string[] dataSets;
+            string dataOfDay = string.Empty;
+
+            dataSets = result.Split(delimiters1, StringSplitOptions.RemoveEmptyEntries);
+
+            int row = 1;
+            Boolean resultDayFound = false;
+            foreach (string s in dataSets)
+            {
+                if (s.Contains("03.08.17"))
+                {
+                    dataOfDay = s;
+                    //lbl_result.Text = "Ergebnis " + row + ":   " + s;
+                    resultDayFound = true;
+                }
+
+                //lbl_result.Text += row.ToString() + ":   " + s + '\n';
+                row++;
+            }
+
+
+
+
+            if (!resultDayFound)
+            {
+                lbl_result.Text = "Kein Notdienst registriert.";
+            } else
+            {
+
+
+
+                // Datensatz der dritten und vierten Spalte extrahieren
+
+
+
+                string[] delimiters2 = new string[] { "<TD ALIGN=LEFT VALIGN=TOP><FONT SIZE=1 COLOR=\"#00CC00\">", "<TD ALIGN=LEFT VALIGN=TOP><FONT SIZE=1 COLOR=\"#FF0000\">", "</FONT></TD>" };
+
+                string dataOfFirst = string.Empty;
+                string dataOfSecond = string.Empty;
+
+                dataSets = dataOfDay.Split(delimiters2, StringSplitOptions.RemoveEmptyEntries);
+
+                int dayRow = 1;
+                foreach (string s in dataSets)
+                {
+                    if (dayRow == 4)
+                    {
+                        dataOfFirst = s;
+                    } else if (dayRow == 6)
+                    {
+                        dataOfSecond = s;
+                    }
+                    
+                    dayRow++;
+                }
+
+                lbl_result.Text = "9 - 9 Uhr:    " + dataOfFirst + '\n';
+                lbl_result.Text += "9 - 22 Uhr:  " + dataOfSecond;
+            }
+
+            
+
+
+
+            //lbl_result.Text = result;
+
+        }
+    }
+}
